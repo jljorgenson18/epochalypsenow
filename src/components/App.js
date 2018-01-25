e2c0e87ba65e2bac9b8b0a5f7dfea99667e08294
Ana Goodwin <anag@criticalmass.com>
2018-01-24 22:39:54 -0600

    adding timestamp input

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import { normalize } from 'polished';
+import styled, { injectGlobal } from 'styled-components';
 import { hot } from 'react-hot-loader';
+import parse from 'date-fns/parse';
+import { unformat } from 'accounting';
 
-const TestComponent = styled.p`
-  color: tomato;
+injectGlobal`
+  ${normalize()}
+
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  * {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
+const Wrapper = styled.main`
+  background-color: #fff2f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
 
 class App extends Component {
+  state = {
+    timestamp: '',
+    convertedDate: null
+  };
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({ timestamp: value ? unformat(value).toString() : '' });
+  };
+
+  handleSubmit = event => {
+    this.setState({ convertedDate: parse(Number(this.state.timestamp)) });
+    event.preventDefault();
+  };
+
   static propTypes = {};
 
   render() {
+    const convertedDate = this.state.convertedDate;
+
     return (
-      <div>
+      <Wrapper>
         <h1>Let us begin??</h1>
-        <TestComponent>Hey I am a styled component</TestComponent>
-      </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.timestamp}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {convertedDate ? <h5>{convertedDate.toString()}</h5> : null}
+        {convertedDate ? <h5>{convertedDate.toISOString()}</h5> : null}
+      </Wrapper>
     );
   }
 }
