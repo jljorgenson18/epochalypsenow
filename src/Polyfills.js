cbf13a4056ddd0b21ac9bd625afab81fe975f4d7
Jesse Jorgenson <jljorgenson18@gmail.com>
2018-07-22 16:36:31 -0500

    Removing fun facts

--- src/Polyfills.js
+++ src/Polyfills.js
@@ -1,7 +1,3 @@
 /*eslint no-var:0*/
 import 'babel-polyfill'; // This is set from babel-preset-env
-
 import 'whatwg-fetch';
-
-import finallyShim from 'promise.prototype.finally/shim';
-finallyShim();
