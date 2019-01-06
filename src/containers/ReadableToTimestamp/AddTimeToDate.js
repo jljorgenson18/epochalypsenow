74ff8e0c08e61a0e7749b72a8d24db356d1d9d34
Ana Goodwin <ana.goodwin10@gmail.com>
2019-01-05 18:14:44 -0600

    resolve conflict

--- src/containers/ReadableToTimestamp/AddTimeToDate.js
+++ src/containers/ReadableToTimestamp/AddTimeToDate.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-<<<<<<< HEAD
 import ExpandIcon from '@material-ui/icons/Add';
 import CollapseIcon from '@material-ui/icons/Remove';
-=======
-import AddIcon from '@material-ui/icons/Add';
 import { translate } from 'react-i18next';
->>>>>>> 2096b18471fcfa7b926db581d204c7e308801832
 
 import Button from '../../components/Button';
 import StyledSelect from '../../components/formControls/Select';
@@ -116,13 +112,8 @@ class AddTimeToDate extends Component {
     return (
       <Wrapper className={expanded ? 'open' : ''}>
         <button className="accordionTrigger" onClick={this.handleClick}>
-<<<<<<< HEAD
           Modify Date
           {expanded ? <CollapseIcon /> : <ExpandIcon />}
-=======
-          {t('Modify Date')}
-          <AddIcon />
->>>>>>> 2096b18471fcfa7b926db581d204c7e308801832
         </button>
         <form onSubmit={this.handleSubmit} className={expanded ? 'open' : ''}>
           <StyledSelect
